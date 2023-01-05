9af3be0387ddea73bf7486d142317ed5f6021d71
Miles Mao <mrmao1@gmail.com>
2023-01-05 13:02:47 -0600

    methods overload exercise results

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -3,8 +3,36 @@
     public class Program
     {
         static void Main(string[] args)
+
         {
-            Console.WriteLine("Hello, World!");
+
+
         }
+
+             public static int Add(int x, int y)
+              {
+                return (x + y);
+              }      
+        
+              public static decimal Add(decimal a, decimal b) 
+              {
+                return a + b;
+              }
+        
+              public static string Add(int num1, int num2, bool isTrue) 
+              {
+                 var sum = num1 + num2;
+                 
+                    if (isTrue && sum > 1) 
+            
+                    {
+                        return $"{sum} dollars";
+                    }
+                    else if (isTrue && sum == 1) 
+                    {
+                        return $"{sum} dollar";
+                    }
+
+              }
     }
 }
